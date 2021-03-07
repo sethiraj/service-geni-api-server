f8994666512857a4470c26d2f7f26b2329dfafb6
Surendran E <surendrane@xpanxion.com>
2021-03-07 12:36:50 +0530

    Added Cross Origin

--- src/main/java/com/mahelinc/servicegenie/controller/ServiceGenieApiServiceController.java
+++ src/main/java/com/mahelinc/servicegenie/controller/ServiceGenieApiServiceController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ import io.swagger.annotations.Api;
 @RestController
 @RequestMapping("/api/v1")
 @Api(value = "/api/v1")
+@CrossOrigin
 public class ServiceGenieApiServiceController {
 
 	/** The garage service. */
