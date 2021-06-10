1198b473a8f8ada6ae22d0add6f7dbd191d6c440
Your Name <you@example.com>
2021-06-10 19:45:22 +0530

    Added basic authentication with https

--- src/main/java/com/mahelinc/servicegenie/security/SecurityConfig.java
+++ src/main/java/com/mahelinc/servicegenie/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @author surendrane
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	/**
@@ -25,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.requiresChannel().anyRequest().requiresSecure().and().httpBasic().and().headers().httpStrictTransportSecurity().disable();
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
 	}
 
 	/**
